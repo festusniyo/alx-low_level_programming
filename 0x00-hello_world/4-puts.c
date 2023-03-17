953efbd579675757b4684b6a42e9b54f6334e413
NIYONGABO Festus <festusniyongabo@gmail.com>
2023-03-17 01:08:23 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -function main contain our c program
+ *
+ * return: equal zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
