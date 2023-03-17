72a7b5aa6a6e7679b17c0a80a043467bb102518f
NIYONGABO Festus <festusniyongabo@gmail.com>
2023-03-17 01:32:13 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function that contain our program code
+ *
+ * Return: 0 after successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
