d196f77f19f74fafc932910c5ca04418c69a9436
NIYONGABO Festus <festusniyongabo@gmail.com>
2023-03-18 10:16:47 -0700

    digit in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits in base 10
+ *
+ * Return: 0 returned
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
