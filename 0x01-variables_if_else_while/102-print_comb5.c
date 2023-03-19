16730dcaf2e712e386ca10df03dd50350a827003
NIYONGABO Festus <festusniyongabo@gmail.com>
2023-03-19 08:00:37 -0700

    combination of two two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,17 +13,20 @@ int main(void)
 
 	for (a = 0; a <= 98; a++)
 	{
-		for (b = a + 1; b <= 99; b++)
+		for (b = 0; b <= 99; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(' ');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-			if (a != 17 && b != 18)
+			if (a < b)
 			{
-				putchar(',');
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
 				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
