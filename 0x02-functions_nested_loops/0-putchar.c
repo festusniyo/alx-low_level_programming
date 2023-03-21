15694387fdc9cda580689a93be6f3a1a24b872a9
NIYONGABO Festus <festusniyongabo@gmail.com>
2023-03-21 01:25:41 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - function contain a program that print _putchar alphabet
+ * Return: 0 returned
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
