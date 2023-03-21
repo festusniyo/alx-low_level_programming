4e5c152adfe672c8e338a61afac85888a8847bf5
NIYONGABO Festus <festusniyongabo@gmail.com>
2023-03-21 03:04:19 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that print 10 times of alphabet
+ *
+ * Return: 0 returned
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
+
