0a4b9ff1f5da19d75fd91f00fab562521383f6f7
NIYONGABO Festus <festusniyongabo@gmail.com>
2023-03-21 05:37:07 -0700

    check if alphabet is lower case or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * islower - a function that return 1 if alphabet is lower and 0 if not
+ *
+ * Return: when is lower case return 1 otherwise o
+ *
+ * @c: parameter to be printed
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
