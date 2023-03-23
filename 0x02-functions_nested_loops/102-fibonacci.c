9d2a86f46bf1096994c36fc4be964507ea70cda6
NIYONGABO Festus <festusniyongabo@gmail.com>
2023-03-23 01:43:58 -0700

    50 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - a function that printing the first fibonacci numbers
+ *
+ * Return: 0 returned
+ */
+
+int main(void)
+{
+	int x = 0;
+	long y = 1;
+	long z = 2;
+
+	while (x < 50)
+	{
+		if (x == 0)
+			printf("%ld", y);
+		else if (x == 1)
+		{
+			printf("%ld", z);
+		}
+		else
+		{
+			z += y;
+			y = z - y;
+			printf(", %ld", z);
+		}
+		++x;
+	}
+	printf("\n");
+	return (0);
+}
